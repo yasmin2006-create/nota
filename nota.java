d9218bb8f2c82268ff622417e5e716f0ff583255
yasmin2006-create <yasminlouisefontes1507@gmail.com>
2026-03-30 22:46:34 -0300

    Refactor nota.java for improved readability

--- nota.java
+++ nota.java
@@ -1,22 +1,18 @@
+//YASMIN LOUISE
+//1261935138
 import java.util.Scanner;
-
 public class nota {
 public static void main(String[] args) {
  Scanner sc = new Scanner(System.in);
-
  double nota;
-
 System.out.print("Digite a nota (0 a 100): ");
  nota = sc.nextDouble();
-
-
 while (nota < 0 || nota > 100) {
 System.out.println("Nota inválida. Tente novamente.");
  System.out.print("Digite a nota (0 a 100): ");
  nota = sc.nextDouble();
         }
 
-   
 if (nota >= 90) {
 System.out.println("Conceito: A");
 } else if (nota >= 75) {
